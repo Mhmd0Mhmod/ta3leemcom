6f8b15a46b83d5db37a57482f2788b177b0bccab
AmrMustafa282 <amrdarwish115@gmail.com>
2024-07-30 14:36:37 +0300

    added the instructions page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,24 @@
+// import Button from "../components/ui/Button";
+// import { ArrowLeft } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+ return (
+  <>
+   <div>Home</div>
+   {/* <div className="flex gap-4 w-full justify-center">
+    <Button type={"primary"} className={""}>
+     المدرسين
+    </Button>
+    <Button type={"primary"} icon={<ArrowLeft  />} className={""}>
+     انضم الينا
+    </Button>
+    <Button type={"outline"} className={""}>
+     الطلاب
+    </Button>
+    <Button type={"ghost"} className={""}>
+     تسجيل الدخول
+    </Button>
+   </div> */}
+  </>
+ );
 }
