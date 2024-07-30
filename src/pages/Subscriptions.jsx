2b774a5fe489f3c22c900d49979678f6eb67920e
Mohamed Mahmoud <112190828+Mhmd0Mhmod@users.noreply.github.com>
2024-07-30 14:58:59 +0300

    start Subscriptions

--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,6 +1,18 @@
-
 export default function Subscriptions() {
-  return (
-    <div>subscriptions</div>
-  )
+    return (
+        <div className={"relative mt-5 "}>
+            <div className={"w-[50%] absolute left-0 z-0"}>
+                <img src="../../public/circles.svg" alt="circles" className={"w-[100%]"}/>
+            </div>
+                <div className={"w-[10%] absolute right-5 "}>
+                    <img src="../../public/lamp.svg" alt="lamp" className={"w-[70%]"}/>
+                    <p className={"text-gray-400"}>لا تقلق حول التكلفه</p>
+                </div>
+            <div className={"relative z-10"}>
+                <div className={" "}>
+                    <h1 className={"mx-auto w-fit text-3xl font-bold  "}>الاشتركات</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
