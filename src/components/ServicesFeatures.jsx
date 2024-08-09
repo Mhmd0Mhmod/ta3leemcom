af6bb138700111397cdb452b8ecab8b178b491c0
Mohamed Mahmoud <112190828+Mhmd0Mhmod@users.noreply.github.com>
2024-08-09 23:44:39 +0300

    Done !

--- src/components/ServicesFeatures.jsx
+++ src/components/ServicesFeatures.jsx
@@ -7,9 +7,9 @@ export default function ServicesFeatures({ name, arr, isAvailable = false }) {
         <div className="mt-5 flex gap-x-4 ms-16 font-cairo">
             <div className="border-l-[1px] p-4 w-[300px] h-[143px] relative">
                 <img src={rect} alt="" />
-                <h2 className="font-cairo-bold text-[24px] ms-6 w-[251px] ">{name}</h2>
+                <h2 className="font-cairo-bold text-[24px] ms-6 w-[204px] ">{name}</h2>
                 
-                <span className="absolute top-2 -left-8 text-[64px] -rotate-45">
+                <span className="absolute top-5 -left-16 text-[64px] -rotate-45">
                     {isAvailable ? <p>قريبا</p> : null}
                 </span>
             </div>
