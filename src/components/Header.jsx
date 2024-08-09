1a3c3d060122e1d030a81589b1e86551a07edd89
Mohamed Mahmoud <112190828+Mhmd0Mhmod@users.noreply.github.com>
2024-08-09 17:45:01 +0300

    Finish Header hover and edit login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,17 +13,17 @@ export default function Header() {
             <div className={"flex"}>
                 <img src={logo} alt={"Icon"} />
                 <ul className={"w-[922px] flex items-center justify-between gap-[12px] text-[32px]"}>
-                    <li className={currentPath === "/home" ? "active" : ""}><Link to="/home">الرئيسية</Link></li>
-                    <li className={currentPath === "/about" ? "active" : ""}><Link to="/about">عن</Link></li>
-                    <li className={currentPath === "/services" ? "active" : ""}><Link to="/services">الخدمات</Link></li>
-                    <li className={currentPath === "/instructions" ? "active" : ""}><Link to="/instructions">تعليمات</Link></li>
-                    <li className={currentPath === "/subscriptions" ? "active" : ""}><Link to="/subscriptions">الاشتركات</Link></li>
-                    <li className={currentPath === "/opinion" ? "active" : ""}><Link to="/opinion">رأيك</Link></li>
-                    <li className={currentPath === "/contact-with-us" ? "active" : ""}><Link to="/contact-with-us">تواصل معنا</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/home" ? "active" : ""}`}><Link to="/home">الرئيسية</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${ currentPath === "/about" ? "active" : ""}`}><Link to="/about">عن</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/services" ? "active" : ""}`}><Link to="/services">الخدمات</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/instructions" ? "active" : ""}`}><Link to="/instructions">تعليمات</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/subscriptions" ? "active" : ""}`}><Link to="/subscriptions">الاشتركات</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/opinion" ? "active" : ""}`}><Link to="/opinion">رأيك</Link></li>
+                    <li className={`hover:text-[#0884A2] duration-500 ${currentPath === "/contact-with-us" ? "active" : ""}`}><Link to="/contact-with-us">تواصل معنا</Link></li>
                 </ul>
             </div>
             <div className={"flex items-center gap-[10px]"}>
-                <Button type={"ghost"}>تسجيل الدخول</Button>
+                <Button type={"ghost"} className={"border-0"}>تسجيل الدخول</Button>
                 <Button type={"primary"} className={"flex items-center gap-2"}>
                     انضم إلينا
                     <Icon src={'../../public/Icons/leftArrow.svg'} className={"w-7"} />
