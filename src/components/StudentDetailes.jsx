43e39dd66ee393aa5c6dd039794c089289faa807
Salah Muhamad <144562089+Salah-Muhamad@users.noreply.github.com>
2024-08-24 02:31:50 +0300

    finish student details

--- src/components/StudentDetailes.jsx
+++ src/components/StudentDetailes.jsx
@@ -18,24 +18,24 @@ function StudentDetailes() {
             <div>
               <h3 className="text-3xl font-almaria-bold mt-6">بيانات الطالب</h3>
               <div className="flex gap-6 mt-6 mb-8">
-                <button className={`bg-[#0884A2] text-white rounded-lg w-28 h-10 text-xl`}>تعديل</button>
-                <button className={`bg-[#F54547] text-white rounded-lg w-28 h-10 text-xl`}>حذف</button>
+                <button className={`bg-[#0884A2] text-white rounded-lg w-28 h-10 text-xl font-almaria-bold`}>تعديل</button>
+                <button className={`bg-[#F54547] text-white rounded-lg w-28 h-10 text-xl font-almaria-bold`}>حذف</button>
               </div>
-              <form action="">
+              <form>
                 <h3 className="text-lg font-almaria-bold mb-4">الاسم بالكامل</h3>
-                <input type="text" className="bg-[#EFEFEF] w-[43.75rem] h-10 p-2" value={studentName} onChange={(e)=>setStudentName(e.target.value)}/>
+                <input type="text" className="bg-[#EFEFEF] w-[43.75rem] h-10 p-2 text-[1.125rem] rounded-lg" value={studentName} onChange={(e)=>setStudentName(e.target.value)}/>
                 <div className="flex gap-10 mt-10">
                   <div>
                     <h3 className="text-lg font-almaria-bold mt-8 mb-4">المرحلة الدراسية</h3>
-                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2" value={studentMainLevel} onChange={(e)=>setStudentMainLevel(e.target.value)}/>
+                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2 text-[1.125rem] rounded-lg" value={studentMainLevel} onChange={(e)=>setStudentMainLevel(e.target.value)}/>
                   </div>
                   <div>
                     <h3 className="text-lg font-almaria-bold mt-8 mb-4"> الصف الدراسي</h3>
-                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2" value={studentSubLevel} onChange={(e)=>setStudentSubLevel(e.target.value)}/>
+                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2 text-[1.125rem] rounded-lg" value={studentSubLevel} onChange={(e)=>setStudentSubLevel(e.target.value)}/>
                   </div>
                   <div>
                     <h3 className="text-lg font-almaria-bold mt-8 mb-4"> المجموعة</h3>
-                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2" value={studentGroup} onChange={(e)=>setStudentGroup(e.target.value)}/>
+                    <input type="text" className="bg-[#EFEFEF] w-52 h-10 p-2 text-[1.125rem] rounded-lg" value={studentGroup} onChange={(e)=>setStudentGroup(e.target.value)}/>
                   </div>
                 </div>
               </form>
@@ -44,8 +44,9 @@ function StudentDetailes() {
             {/* ------------- */}
             <div>
               <div >
-                <img src="../../public/imgs/id.svg" alt="Id" />
-                <div className="border-2 rounded-b-md border-t-0 p-4 bg-[#EEEEEE]  relative">
+                <img src="../../public/imgs/id.svg" alt="Id"/>
+                <div>
+                  <div className="border-2 border-[#B0B0B0] rounded-b-md border-t-0 p-4 bg-[#EEEEEE]   relative   bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('../../public/imgs/id-bg.svg')" }}>
                   <div className="w-[5.625rem] h-[5.625rem] bg-[#D9D9D9] absolute left-10 top-[-2.813rem] rounded-full border-[0.313rem] border-white flex justify-center ">
                     <img src="../../public/imgs/profile.svg" alt="profile"  className="w-[50%]" />
                   </div>
@@ -53,22 +54,23 @@ function StudentDetailes() {
                   <p className="text-center text-[#979797] text-lg mt-4 mb-6">{student.code}</p>
                   <div>
                       <div className="flex text-lg">
-                        <div className="w-40 mt-2">الاسم</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.name}</div>
+                        <div className="w-40 mt-2 font-almaria-bold">الاسم</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.name}</div>
                       </div>
                       
                       <div className="flex  text-lg">
-                        <div className="w-40 mt-2">المجموعة</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.group}</div>
+                        <div className="w-40 mt-2 font-almaria-bold">المجموعة</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.group}</div>
                       </div>
                       
                       <div className="flex  text-lg">
-                        <div className="w-40 mt-2">المرحلة الدراسية</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.mainLevel}</div>
+                        <div className="w-40 mt-2 font-almaria-bold">المرحلة الدراسية</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.mainLevel}</div>
                       </div>
                       
                       <div className="flex  text-lg">
-                        <div className="w-40 mt-2">الصف الدراسي</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.subLevel}</div>
+                        <div className="w-40 mt-2 font-almaria-bold">الصف الدراسي</div> <span className="mt-2">:</span> <div className="mt-3 mr-3 text-sm text-[#979797]">{student.subLevel}</div>
                       </div>
                       
                   </div>
+                </div>
                 </div>
               </div>
             </div>
