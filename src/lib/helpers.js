9603085ad1a79c9e65c87b4d0dca9b8b2fa4ef8e
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-09-29 21:03:42 +0300

    create levels context

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,4 +1,16 @@
-export const handleBack=(e)=>{
-  e?.preventDefault();
-  window.history.back();
+import axios from "axios";
+import Cookies from "js-cookie";
+
+export const handleBack = (e) => {
+    e?.preventDefault();
+    window.history.back();
+}
+
+export async function fetchLevels() {
+    const token = Cookies.get('_auth');
+    return  await axios.get(`${import.meta.env.VITE_API_URL}/LevelYear`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
 }
